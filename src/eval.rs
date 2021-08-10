c0ae89aaeff705568bb7ae386f45dfe58b7336d9
Štěpán Mikéska <haroldvemeno@email.cz>
2021-08-10 21:01:06 +0200

    Fixed evaluation order, alpha reduction

--- src/eval.rs
+++ src/eval.rs
@@ -1,53 +1,179 @@
-use std::collections::HashSet;
-use std::error::Error;
+use core::panic;
 use empty_box::EmptyBox;
+use std::collections::HashSet;
+use std::{ascii, error, fmt};
 
-use crate::types::Expr;
+use crate::expr::Expr;
 
-pub fn reduce_nolog(mut expr: Box<Expr>) -> Result<Box<Expr>, Box<dyn Error>> {
-    let max_repetitions = 10000;
-    let max_size = 10000;
-
-    for _ in 1..max_repetitions {
-        expr = do_reduce_nolog(expr);
-        // TODO: actual checks and stuff
-    }
-    Ok(expr)
+#[derive(Clone)]
+pub struct EvalError {
+    msg: String,
 }
 
-fn do_reduce_nolog(expr: Box<Expr>) -> Box<Expr> {
+pub fn reduce_nolog(mut expr: Box<Expr>) -> Result<Box<Expr>, Box<dyn error::Error>> {
+    let max_iterations = 1000;
+    let step = 1;
+    let max_size = 1000000;
+
+    for i in (1..=max_iterations).step_by(step) {
+        for s in 1..=step {
+            expr = match do_reduce_nolog(expr) {
+                (e, true) => e,
+                (e, false) => {
+                    println!("Took {} iterations", i + s);
+                    return Ok(e);
+                }
+
+            };
+        }
+        //println!("{}", expr);
+        let expr_size = size(&expr);
+        if expr_size > max_size {
+            return Err(EvalError::boxed(format!(
+                "Size outgrew maximum size: {} out of {}",
+                expr_size, max_size
+            )));
+        }
+    }
+    Err(EvalError::boxed(format!(
+        "Iteration limit reached: {}",
+        max_iterations
+    )))
+}
+
+fn do_reduce_nolog(expr: Box<Expr>) -> (Box<Expr>, bool) {
+    //println!("do_reduce_nolog: {}", expr);
+
+    use Expr::*;
+    let (ex, eb) = EmptyBox::take(expr);
+    // TODO: recursive -> iterative for trivial cases
+    let (result, reduced) = match ex {
+        // Irreducable
+        Variable(_) => (ex, false),
+        Name(_) => (ex, false), // TODO: name binding and resolution
+
+        // Eta reduction:
+        //   Reduce(\a.Ea)
+        //   a is not free in E
+        //=> Reduce(E)
+        Abstr(var, box Appl(rest, box Variable(last)))
+            if var == last && !unbounds_in(&rest).contains(&var) =>
+        {
+            (*do_reduce_nolog(rest).0, true)
+        }
+        //   Reduce[\a.E]  =>  \a.Reduce[E]
+        Abstr(var, body) => {
+            let (e, r) = do_reduce_nolog(body);
+            (Abstr(var, e), r)
+        }
+
+        // Beta reduction:
+        //   Reduce[(\x.A)B]  => Reduce[A[x->B]]
+        Appl(box Abstr(from, body), to) => (*beta_reduce(body, from, to), true),
+        //   Reduce[AB]
+        Appl(a, to) => {
+            let (red_box, is_red) = do_reduce_nolog(a);
+            let (reduced_a, red_eb) = EmptyBox::take(red_box);
+            match reduced_a {
+                //   if Reduce[A] => \x.C
+                //        Beta reduction:
+                //        Reduce[AB] => Reduce[(\x.C)B] => Reduce[C[x->B]]
+                Abstr(from, body) => (*beta_reduce(body, from, to), true),
+                //   else Reduce[AB] => (Reduce[A])(Reduce[B])
+                other => {
+                    let (e, r) = do_reduce_nolog(to);
+                    (Appl(red_eb.put(other), e), r || is_red)
+                }
+            }
+        }
+    };
+    (eb.put(result), reduced)
+}
+
+fn beta_reduce(expr: Box<Expr>, from: u8, to: Box<Expr>) -> Box<Expr> {
+    let mut unbounds_to = unbounds_in(&to);
+    unbounds_to.insert(from);
+
+    //println!("beta_reduce: {}", expr);
+    //println!("  from: {}", ascii::escape_default(from).to_string());
+    //println!("  to  : {}", to);
+
+    //print!("  unbound in to: ");
+    //for &a in unbounds_to.iter() {
+    //    print!("{}", ascii::escape_default(a).to_string())
+    //}
+    //println!();
+
+    fn beta(
+        expr: Box<Expr>,
+        rest @ (from, to, to_unb): &(u8, Box<Expr>, HashSet<u8>),
+    ) -> Box<Expr> {
+        use Expr::*;
+        let (ex, eb) = EmptyBox::take(expr);
+        eb.put(match ex {
+            Name(_) => ex,
+            Appl(a, b) => Appl(beta(a, rest), beta(b, rest)),
+            Abstr(v, b) => {
+                if v == *from {
+                    Abstr(v, b)
+                } else if to_unb.contains(&v) {
+                    let (v, e) = alpha(v, b, to_unb);
+                    Abstr(v, beta(e, rest))
+                } else {
+                    Abstr(v, beta(b, rest))
+                }
+            }
+            Variable(v) => {
+                if v == *from {
+                    *to.clone()
+                } else {
+                    ex
+                }
+            }
+        })
+    }
+
+    beta(expr, &(from, to, unbounds_to))
+}
+
+fn alpha_next(taken: &HashSet<u8>) -> u8 {
+    for letter in b'a'..=b'z' {
+        if !taken.contains(&letter) {
+            return letter;
+        }
+    }
+    panic!("Ran out of variables");
+}
+
+fn alpha(par: u8, body: Box<Expr>, taken: &HashSet<u8>) -> (u8, Box<Expr>) {
+    let unused = alpha_next(taken);
+    (unused, replace_var(body, par, unused))
+}
+
+fn replace_var(expr: Box<Expr>, from: u8, to: u8) -> Box<Expr> {
     use Expr::*;
     let (ex, eb) = EmptyBox::take(expr);
     eb.put(match ex {
-        Variable(_) | Name(_) => ex,
-        Abstr(var, box Appl(rest, box Variable(last))) if var == last => {
-            *do_reduce_nolog(rest)
+        Name(_) => ex,
+        Appl(a, b) => Appl(replace_var(a, from, to), replace_var(b, from, to)),
+        Abstr(v, b) => {
+            if v == from {
+                Abstr(v, b)
+            } else {
+                Abstr(v, replace_var(b, from, to))
+            }
         }
-        Abstr(var, body) => {
-            Abstr(var, do_reduce_nolog(body))
-        }
-        Appl(box Abstr(from, body), to) => {
-            *do_reduce_nolog(beta_reduce(body, from, to))
-        }
-        Appl(a, to) => {
-            let (reduced_a, red_box) = EmptyBox::take(do_reduce_nolog(a));
-            match reduced_a {
-                Abstr(from, body) => 
-                    *do_reduce_nolog(beta_reduce(body, from, to)),
-                other => Appl(red_box.put(other), do_reduce_nolog(to))
+        Variable(v) => {
+            if v == from {
+                Variable(to)
+            } else {
+                ex
             }
         }
     })
 }
 
-fn beta_reduce(expr: Box<Expr>, from: u8, to: Box<Expr>) -> Box<Expr> {
-    let bound_in_expr = free_variables(&expr);
-    let bound_in_to = free_variables(&to);
-
-    unimplemented!("beta_reduce is not implemented yet")
-}
-
-fn free_variables(expr: &Expr) -> HashSet<u8> {
+fn unbounds_in(expr: &Expr) -> HashSet<u8> {
     match expr {
         Expr::Variable(v) => {
             let mut set = HashSet::new();
@@ -56,16 +182,57 @@ fn free_variables(expr: &Expr) -> HashSet<u8> {
         }
         Expr::Name(_) => HashSet::new(),
         Expr::Abstr(v, b) => {
-            let mut set = free_variables(b);
+            let mut set = unbounds_in(b);
             set.remove(v);
             set
         }
         Expr::Appl(a, b) => {
-            let mut set = free_variables(a);
-            for item in free_variables(b) {
+            let mut set = unbounds_in(a);
+            for item in unbounds_in(b) {
                 set.insert(item);
             }
             set
         }
-    }   
+    }
 }
+
+fn size(expr: &Expr) -> u32 {
+    let mut ret = 0;
+    let mut stack = Vec::new();
+    let mut next = expr;
+    loop {
+        ret += 1;
+        next = match next {
+            Expr::Variable(_) | Expr::Name(_) => match stack.pop() {
+                Some(e) => e,
+                None => break,
+            },
+            Expr::Abstr(_, body) => body,
+            Expr::Appl(a, b) => {
+                stack.push(a);
+                b
+            }
+        }
+    }
+    ret
+}
+
+impl EvalError {
+    fn boxed(msg: impl Into<String>) -> Box<EvalError> {
+        Box::new(EvalError { msg: msg.into() })
+    }
+}
+
+impl fmt::Display for EvalError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "EvalError: {}", self.msg)
+    }
+}
+
+impl fmt::Debug for EvalError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "EvalError: {}", self.msg)
+    }
+}
+
+impl error::Error for EvalError {}
