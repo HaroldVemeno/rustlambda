1813dbeb7bf91c7447dcf221e7d252c14990eab6
Štěpán Mikéska <haroldvemeno@email.cz>
2021-08-10 21:00:50 +0200

    Moving things around

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,41 @@
 #![feature(box_patterns)]
 
-use std::io;
-use std::fs::File;
-use std::error::Error;
 use std::env;
+use std::error::Error;
+use std::fs::File;
+use std::io;
 
 mod eval;
 mod lex;
 mod parse;
-mod print;
-mod types;
-
+mod expr;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let vec: Vec<_> = env::args().collect();
-    let lexed = if vec.len() == 1 {
-        let input = io::stdin();
-        eprintln!("Lexing:\n");
-        lex::lex(input)?
+    let lexed = match vec.len() {
+        1 => {
+            let input = io::stdin();
+            eprintln!("Lexing:\n");
+            lex::lex(input)?
+        }
+        _ => {
+            let input = File::open(&vec[1])?;
+            eprintln!("Lexing:\n");
+            lex::lex(input)?
+        }
+    };
+    //eprintln!("{:?}", lexed);
 
-    } else if vec.len() > 1 {
-        let input = File::open(&vec[1])?;
-        eprintln!("Lexing:\n");
-        lex::lex(input)?
-    } else {unreachable!()};
-
-
-    eprintln!("{:?}", lexed);
-
-    eprintln!("Parsing:\n");
+    eprintln!("Parsing\n");
     let parsed = parse::parse(lexed)?;
-    eprintln!("{:#}\n", parsed);
-    eprintln!("{:?}\n", parsed);
-    eprintln!("{}\n", parsed);
+    // eprintln!("{:#}\n", parsed);
+    // eprintln!("{:?}\n", parsed);
+    // eprintln!("{}\n", parsed);
 
-    eprintln!("Evaluating:\n");
+    eprintln!("Evaluating\n");
     let evaluated = eval::reduce_nolog(parsed)?;
-    eprintln!("{:#}\n", evaluated);
-    eprintln!("{:?}\n", evaluated);
+    // eprintln!("{:#}\n", evaluated);
+    // eprintln!("{:?}\n", evaluated);
     eprintln!("{}\n", evaluated);
 
     Ok(())
