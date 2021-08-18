c898afa616f7f6d785529998bd3b1d630a7302f9
Štěpán Mikéska <haroldvemeno@email.cz>
2021-08-18 14:20:26 +0200

    Benchmarks in criterion

---
+++ benches/benchmark.rs
@@ -0,0 +1,32 @@
+#![feature(box_patterns)]
+
+use criterion::*;
+
+use rustlambda::{eval, lex, parse};
+
+pub fn bench_stuff(c: &mut Criterion) {
+    let input = include_bytes!("../res/test_stuff");
+    {
+        c.bench_function("lex", |b| b.iter(|| lex::lex(&input[..]).unwrap()));
+    }
+    let lexed = lex::lex(&input[..]).unwrap();
+    c.bench_function("parse", |b| b.iter(|| parse::parse(lexed.clone()).unwrap()));
+
+    let parsed = parse::parse(lexed.clone()).unwrap();
+    c.bench_function("eval", |b| b.iter(|| eval::reduce(parsed.clone())));
+}
+
+pub fn bench_rec_factorial(c: &mut Criterion) {
+    let input = include_bytes!("../res/test_rec_factorial");
+    {
+        c.bench_function("lex", |b| b.iter(|| lex::lex(&input[..]).unwrap()));
+    }
+
+    let lexed = lex::lex(&input[..]).unwrap();
+    c.bench_function("parse", |b| b.iter(|| parse::parse(lexed.clone()).unwrap()));
+
+    let parsed = parse::parse(lexed.clone()).unwrap();
+    c.bench_function("eval", |b| b.iter(|| eval::reduce(parsed.clone())));
+}
+criterion_group!(benches, bench_stuff, bench_rec_factorial);
+criterion_main!(benches);
