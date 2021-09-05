4eba9918db2ac98291a28c91e4738d502cd59a20
Štěpán Mikéska <haroldvemeno@email.cz>
2021-09-05 22:53:06 +0200

    More benches

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,56 +1,82 @@
+use std::time::Duration;
+
 use criterion::*;
 
 #[allow(unused_imports)]
 use rustlambda::{
     eval,
-    expr::{self, Expr, Defs},
+    expr::{self, Defs, Expr, expr_aliases::*},
     lex, parse,
 };
 
+fn setup<M: measurement::Measurement>(gr: &mut BenchmarkGroup<M>) {
+    gr.warm_up_time(Duration::from_millis(500));
+    gr.sample_size(20);
+}
+
 pub fn bench_stuff(c: &mut Criterion) {
+    let mut gr = c.benchmark_group("stuff");
+    setup(&mut gr);
     let input = include_bytes!("../res/test_stuff");
-    {
-        c.bench_function("lex stuff", |b| b.iter(|| lex::lex(&input[..]).unwrap()));
-    }
+    gr.bench_function("lex stuff", |b| b.iter(|| lex::lex(&input[..]).unwrap()));
     let lexed = lex::lex(&input[..]).unwrap();
-    c.bench_function("parse stuff", |b| {
+    gr.bench_function("parse stuff", |b| {
         b.iter(|| parse::parse(lexed.clone()).unwrap())
     });
 
     let parsed = parse::parse(lexed.clone()).unwrap();
     let expr = parsed.1.unwrap();
     let defs = parsed.0;
-    c.bench_function("eval stuff", |b| {
+    gr.bench_function("eval stuff", |b| {
         b.iter(|| eval::reduce(expr.clone(), &defs, false))
     });
 }
 
 pub fn bench_rec_factorial(c: &mut Criterion) {
-    let input = include_bytes!("../res/test_recfact");
     {
-        c.bench_function("lex recursive factorial", |b| {
-            b.iter(|| lex::lex(&input[..]).unwrap())
-        });
+        let mut gr = c.benchmark_group("recursive factorial");
+        setup(&mut gr);
+        let input = include_bytes!("../res/test_recfact");
+        gr.bench_function("lex", |b| b.iter(|| lex::lex(&input[..]).unwrap()));
+
+        let lexed = lex::lex(&input[..]).unwrap();
+        gr.bench_function("parse", |b| b.iter(|| parse::parse(lexed.clone()).unwrap()));
+
+        let parsed = parse::parse(lexed.clone()).unwrap();
+        let defs = parsed.0;
+        for i in 1..=5 {
+            gr.bench_with_input(BenchmarkId::from_parameter(i), &i, |b, &i| {
+                let expr = appl(name("Fact"), church(i));
+                b.iter(|| eval::reduce(expr.clone(), &defs, false))
+            });
+        }
     }
+    {
+        let mut gr = c.benchmark_group("inlined recursive factorial");
+        setup(&mut gr);
+        let input = include_bytes!("../res/test_recfact_inline");
+        gr.bench_function("lex", |b| b.iter(|| lex::lex(&input[..]).unwrap()));
 
-    let lexed = lex::lex(&input[..]).unwrap();
-    c.bench_function("parse recursive factorial", |b| {
-        b.iter(|| parse::parse(lexed.clone()).unwrap())
-    });
+        let lexed = lex::lex(&input[..]).unwrap();
+        gr.bench_function("parse", |b| b.iter(|| parse::parse(lexed.clone()).unwrap()));
 
-    let parsed = parse::parse(lexed.clone()).unwrap();
-    let expr = parsed.1.unwrap();
-    let defs = parsed.0;
-    c.bench_function("eval recursive factorial", |b| {
-        b.iter(|| eval::reduce(expr.clone(), &defs, false))
-    });
+        let parsed = parse::parse(lexed.clone()).unwrap();
+        let defs = parsed.0;
+        for i in 1..=5 {
+            gr.bench_with_input(BenchmarkId::from_parameter(i), &i, |b, &i| {
+                let expr = appl(name("Fact"), church(i));
+                b.iter(|| eval::reduce(expr.clone(), &defs, false))
+            });
+        }
+    }
 }
 
 pub fn bench_exp(c: &mut Criterion) {
-    let mut group = c.benchmark_group("eval exp");
+    let mut gr = c.benchmark_group("eval exp");
+    setup(&mut gr);
     let defs = Defs::new();
     for exp in 1..=10 {
-        group.bench_with_input(BenchmarkId::from_parameter(exp), &exp, |b, &exp| {
+        gr.bench_with_input(BenchmarkId::from_parameter(exp), &exp, |b, &exp| {
             let e = Box::new(Expr::Appl(Expr::church_num(exp), Expr::church_num(2)));
             b.iter(|| eval::reduce(e.clone(), &defs, false));
         });
