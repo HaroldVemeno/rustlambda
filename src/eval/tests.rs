d01b3d1224c876942e655c0e9d429e60478ddb17
Štěpán Mikéska <haroldvemeno@email.cz>
2021-09-06 21:51:51 +0200

    Test test eval test

--- src/eval/tests.rs
+++ src/eval/tests.rs
@@ -2,23 +2,43 @@ use std::collections::HashMap;
 
 use super::*;
 use crate::expr::expr_aliases::*;
-use crate::vappl;
 use crate::test::*;
+use crate::{vabstr, vappl};
 
-fn church(f: &dyn Fn() -> Box<Expr>, bn: u32, pn: u32) -> u32 {
-    let p = Expr::church_num(pn);
-    let b = Expr::church_num(bn);
+fn with_church_nums(f: Box<Expr>, bn: u32, pn: u32) -> Option<u32> {
+    let p = church(pn);
+    let b = church(bn);
 
-    let b_p = vappl!(f(), p, b);
+    let b_p = vappl!(f, p, b);
     let b_p_reduced = reduce(b_p, &HashMap::new(), false).unwrap();
 
-    b_p_reduced
-        .try_unchurch_num()
-        .expect("pow of two church nums turned into something nonchurchnumeric")
+    b_p_reduced.try_unchurch_num()
 }
 
 #[test]
 fn church1() {
-    let id = || abstr(b'a', var(b'a'));
-    assert_eq!(church(&id, 3, 4), 3u32.pow(4));
+    assert_eq!(
+        with_church_nums(abstr(b'a', var(b'a')), 3, 4)
+            .expect("pow of two church nums turned into something nonchurchnumeric"),
+        (3u32).pow(4)
+    );
+    assert_eq!(
+        with_church_nums(
+            vabstr!(
+                b'n',
+                b'm',
+                b'f',
+                b'x',
+                vappl!(var(b'm'), var(b'f'), vappl!(var(b'n'), var(b'f'), var(b'x')))
+            ),
+            3,
+            4
+        )
+        .expect("pow of two church nums turned into something nonchurchnumeric"),
+        3 + 4
+    );
+}
+
+#[test]
+fn stuff() {
 }
