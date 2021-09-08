0dc1171c06bbd3f917ffeb0011459baa80914484
Štěpán Mikéska <haroldvemeno@email.cz>
2021-09-08 21:45:12 +0200

    More tests

--- src/eval/tests.rs
+++ src/eval/tests.rs
@@ -5,40 +5,72 @@ use crate::expr::expr_aliases::*;
 use crate::test::*;
 use crate::{vabstr, vappl};
 
-fn with_church_nums(f: Box<Expr>, bn: u32, pn: u32) -> Option<u32> {
-    let p = church(pn);
-    let b = church(bn);
-
-    let b_p = vappl!(f, p, b);
-    let b_p_reduced = reduce(b_p, &HashMap::new(), false).unwrap();
-
-    b_p_reduced.try_unchurch_num()
+fn red(expr: Box<Expr>) -> Box<Expr> {
+    reduce(expr, &HashMap::new(), false)
+        .unwrap()
 }
 
 #[test]
-fn church1() {
-    assert_eq!(
-        with_church_nums(abstr(b'a', var(b'a')), 3, 4)
-            .expect("pow of two church nums turned into something nonchurchnumeric"),
-        (3u32).pow(4)
-    );
-    assert_eq!(
-        with_church_nums(
-            vabstr!(
-                b'n',
-                b'm',
-                b'f',
-                b'x',
-                vappl!(var(b'm'), var(b'f'), vappl!(var(b'n'), var(b'f'), var(b'x')))
-            ),
-            3,
-            4
+fn church_pow() {
+    let expr = appl(church(4), church(3));
+    let reduced = reduce(expr, &HashMap::new(), false)
+        .unwrap()
+        .try_unchurch_num()
+        .expect("pow of two church nums turned into something nonchurchnumeric");
+    assert_eq!(reduced, (3u32).pow(4));
+}
+
+#[test]
+fn church_add() {
+    let add = vabstr!(
+        b'n',
+        b'm',
+        b'f',
+        b'x',
+        vappl!(
+            var(b'm'),
+            var(b'f'),
+            vappl!(var(b'n'), var(b'f'), var(b'x'))
         )
-        .expect("pow of two church nums turned into something nonchurchnumeric"),
-        3 + 4
     );
+    let expr = vappl!(add, church(6), church(9));
+    let reduced = reduce(expr, &HashMap::new(), false)
+        .unwrap()
+        .try_unchurch_num()
+        .expect("sum of two church nums turned into something nonchurchnumeric");
+    assert_eq!(reduced, 6 + 9);
+}
+
+#[test]
+fn church_mul() {
+    let mul = vabstr!(
+        b'n',
+        b'm',
+        b'f',
+        b'x',
+        vappl!(var(b'm'), appl(var(b'n'), var(b'f')), var(b'x'))
+    );
+    let expr = vappl!(mul, church(6), church(9));
+    let reduced = reduce(expr, &HashMap::new(), false)
+        .unwrap()
+        .try_unchurch_num()
+        .expect("product of two church nums turned into something nonchurchnumeric");
+    assert_eq!(reduced, 6 * 9);
 }
 
 #[test]
 fn stuff() {
+    let expr = red(abstr(b'c', appl(vabstr!(
+        b'b',
+        b'a',
+    appl(
+        var(b'c'),
+        var(b'a')
+    )
+    ), var(b'a'))));
+    eprintln!("{:?}", expr);
+    assert!(expr.alpha_eq(&red(
+        vabstr!(
+        b'c', b'a', appl(var(b'c'), var(b'a')))
+    )));
 }
