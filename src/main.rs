2d52652956455062fe55b3450d27d9a4e38d61b2
Štěpán Mikéska <haroldvemeno@email.cz>
2021-09-13 20:20:02 +0200

    Better printing of information

--- src/main.rs
+++ src/main.rs
@@ -31,14 +31,19 @@ fn main() -> Result<(), Box<dyn Error>> {
     dbg!(&opt);
     match opt {
         Eval { mut files } => {
-            if files.is_empty() {
+            let filecount = files.len();
+            if filecount == 0 {
                 files.push("-".into());
             }
-            for file in files {
-                eprintln!("Lexing...");
+
+            for (i, file) in files.into_iter().enumerate() {
                 let lexed = if file == Path::new("-") {
+                    eprintln!("Processing from stdin ({}/{}):", i+1, filecount);
+                    eprintln!("Lexing...");
                     lex::lex(io::stdin())
                 } else {
+                    eprintln!("Processing {} ({}/{}):", file.to_string_lossy(), i+1, filecount);
+                    eprintln!("Lexing...");
                     lex::lex(File::open(file)?)
                 }?;
                 //eprintln!("{:?}", lexed);
@@ -67,38 +72,5 @@ fn main() -> Result<(), Box<dyn Error>> {
             }
         }
     };
-    Ok(())
-}
-
-fn oldmain() -> Result<(), Box<dyn Error>> {
-    let mut args = env::args();
-    args.next();
-    eprintln!("Lexing...");
-    let lexed = match args.next() {
-        None => lex::lex(io::stdin()),
-        Some(s) => lex::lex(File::open(&s)?),
-    }?;
-    //eprintln!("{:?}", lexed);
-
-    eprintln!("Parsing...");
-    let parsed = parse::parse(lexed)?;
-    let expr = parsed.1.ok_or("No main body to evaluate")?;
-    let defs = parsed.0;
-    //eprintln!("{:#}\n", expr);
-    //eprintln!("{:?}\n", expr);
-    eprintln!("{}\n", expr);
-
-    eprintln!("Evaluating\n");
-    let (eval_res, stats) = eval::reduce(expr, &defs);
-    let evaluated = eval_res?;
-
-    println!("{}\n", evaluated);
-    if let Some(num) = evaluated.try_unchurch_num() {
-        eprintln!("Church num!: {}", num)
-    }
-    eprintln!("{}\n", stats);
-    // eprintln!("{:#}\n", evaluated);
-    // eprintln!("{:?}\n", evaluated);
-
     Ok(())
 }
